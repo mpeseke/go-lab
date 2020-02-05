02ba1426f50c5d217b7740b5330b2bc5de8fc738
Marlon Oliver Peseke <mpeseke@gmail.com>
2020-02-04 21:26:06 -0700

    need to delve into test some more. This I like

--- packCreation/main.go
+++ packCreation/main.go
@@ -2,13 +2,21 @@ package main
 
 import (
 	"fmt"
-	"packCreation/math"
 )
 
 func main() {
 
 	xs := []float64{1, 2, 3, 4}
-	avg := math.Average(xs)
+	avg := Average(xs)
 	fmt.Println(avg)
 
+}
+
+/*Average takes the input of an array of numbers and returns their average. */
+func Average(xs []float64) float64 {
+	total := float64(0)
+	for _, x := range xs {
+		total += x
+	}
+	return total / float64(len(xs))
 }
